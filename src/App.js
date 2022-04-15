608da9b48825df392cc222052af5664c281226b2
Ebenezer Ofere <eoofere@sidmach.com>
2022-04-15 14:22:32 +0100

    11. Fetching data with Hooks

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
-// import React, { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [checked, setChecked] = useState(false);
-
-//   return (
-//     <>
-//       <input
-//         type="checkbox"
-//         value={checked}
-//         onChange={() => setChecked((checked) => !checked)}
-//       />
-//       <p>{checked ? "checked" : "not checked"}</p>
-//     </>
-//   );
-// }
-
-// export default App;
-
-/************************************** SECOND LESSON ***************************************/
-
-// import React, { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [checked, setChecked] = useState(false);
-
-//   function toggle() {
-//     setChecked((checked) => !checked);
-//   }
-
-//   return (
-//     <>
-//       <input type="checkbox" value={checked} onChange={toggle} />
-//       <p>{checked ? "checked" : "not checked"}</p>
-//     </>
-//   );
-// }
-
-// export default App;
-
-/************************************** THIRD LESSON (using useReducer) ***************************************/
-
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
+function App({ login }) {
+  const [data, setData] = useState(null);
 
-  return (
-    <>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </>
-  );
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>;
+  }
+  return <div>No User Available</div>;
 }
 
 export default App;
