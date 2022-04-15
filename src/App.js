5c83dc36be1c48ee341b3ca63db8ff26415d73f2
Ebenezer Ofere <eoofere@sidmach.com>
2022-04-15 18:10:23 +0100

    12. Displaying data from an API

--- src/App.js
+++ src/App.js
@@ -11,9 +11,16 @@ function App({ login }) {
   }, []);
 
   if (data) {
-    return <div>{JSON.stringify(data)}</div>;
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>
+          Twitter at <i>{data.twitter_username}</i>
+        </p>
+        <p>{data.location}</p>
+        <img alt={data.login} src={data.avatar_url} />
+      </div>
+    );
   }
-  return <div>No User Available</div>;
 }
-
 export default App;
