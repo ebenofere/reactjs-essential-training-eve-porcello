24a8dbc220e20905edcc5ef7fc71505329b47909
Ebenezer Ofere <eoofere@sidmach.com>
2022-04-15 18:58:18 +0100

    13. Handling loading states

--- src/App.js
+++ src/App.js
@@ -3,24 +3,38 @@ import "./App.css";
 
 function App({ login }) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
       .then((response) => response.json())
-      .then(setData);
-  }, []);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
 
-  if (data) {
-    return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>
-          Twitter at <i>{data.twitter_username}</i>
-        </p>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
-      </div>
-    );
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
+
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
+
+  if (!data) return null;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>
+        Twitter at <i>{data.twitter_username}</i>
+      </p>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url} />
+    </div>
+  );
 }
 export default App;
