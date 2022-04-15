99a0b07020b9e23a794b5d955f4f166792b31d53
Ebenezer Ofere <eoofere@sidmach.com>
2022-04-15 13:25:15 +0100

    9. Working with useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
+  const [primary, setPrimary] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`He said he feels ${primary}`);
+  }, [primary]);
+
+  useEffect(() => {
+    console.log(`But i don't understand why he also feels ${secondary}`);
+  }, [secondary]);
 
   return (
     <>
-      <h1>Current emotion is {emotion}</h1>
+      <h1>
+        Right now, Eben is <i>{primary}</i> but also <i>{secondary}</i>
+      </h1>
 
-      <button onClick={() => setEmotion("happy")}>Happy</button>
-      <button onClick={() => setEmotion("frustrated")}>Frustrated</button>
-      <button onClick={() => setEmotion("enthusiastic")}>Enthused</button>
+      <button onClick={() => setPrimary("happy")}>Happy</button>
+      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
+      <button onClick={() => setSecondary("awkward")}>Awkward</button>
+      <button onClick={() => setPrimary("frustrated")}>Frustrated</button>
+      <button onClick={() => setPrimary("enthusiastic")}>Enthused</button>
     </>
   );
 }
