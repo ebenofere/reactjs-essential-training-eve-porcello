9c717765ffda4abb7b8342bfe02f26fee8628728
Ebenezer Ofere <eoofere@sidmach.com>
2022-04-15 23:04:05 +0100

    13. Installing React Router 6

---
+++ src/pages.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+/* ----------- Installing React Router 6 ----------- */
+// npm install react-router@next react-router-dom@next
+/* --------------------------------------------------*/
+
+export function Home() {
+  return (
+    <div>
+      <h1>[Company Website]</h1>
+    </div>
+  );
+}
+
+export function About() {
+  return (
+    <div>
+      <h1>[About]</h1>
+    </div>
+  );
+}
+
+export function Events() {
+  return (
+    <div>
+      <h1>[Events]</h1>
+    </div>
+  );
+}
+
+export function Contact() {
+  return (
+    <div>
+      <h1>[Contact]</h1>
+    </div>
+  );
+}
